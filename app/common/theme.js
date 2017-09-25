b38956d8e46f532bcde87c8d8f0ec878b9b17471
Bo Chen <bochen2014@yahoo.com>
2017-09-26 00:05:42 +1000

    main layout

---
+++ app/common/theme.js
@@ -0,0 +1,43 @@
+// @flow
+
+// color
+const color = {
+    blue: '#00A8D8',
+    green: '#9BE13A',
+    text: '#383838',
+    lightText: 'white',
+    sceneBg: '#F4F4F4',
+    splashBg: '#2B2828',
+    yellow: '#FFC979',
+  
+    // neutrals
+    gray90: '#1A1A1A',
+    gray80: '#333',
+    gray70: '#4D4D4D',
+    gray60: '#666',
+    gray50: '#7F7F7F',
+    gray40: '#999',
+    gray35: '#A6A6A6',
+    gray30: '#B3B3B3',
+    gray25: '#BFBFBF',
+    gray20: '#CCC',
+    gray15: '#D9D9D9',
+    gray10: '#E5E5E5',
+    gray05: '#F2F2F2',
+  };
+  
+  // font sizes
+  const fontSize = {
+    xsmall: 12,
+    small: 14,
+    default: 17,
+    large: 24,
+    xlarge: 32,
+  };
+
+
+  export {
+    color,
+    fontSize,
+  };
+  
--- app/search/searchScreen.js
+++ app/search/searchScreen.js
@@ -5,7 +5,10 @@ import {
     Button,
     Text,
     Picker,
+    PixelRatio,
+    StyleSheet,
 } from 'react-native';
+import { color, fontSize } from '../common/theme';
 
 class SearchScreen extends Component {
     go2Details = e => {
@@ -17,14 +20,57 @@ class SearchScreen extends Component {
 
 
     render() {
-        return <View><Text>List of all contacts</Text>
-            
+        return <View style={styles.base}>
+            <View>
+            <View style={[styles.mainBody]}>
+                <Text style={[styles.searchItem]}>Select Make</Text>
+                <Text style={[styles.searchItem, styles.searchOptions]}>Select Make</Text>
+            </View>
+            <View style={[styles.mainBody]}>
+                <Text style={[styles.searchItem]}>Select Model</Text>
+                <Text style={[styles.searchItem,styles.searchOptions]}>Select Model</Text>
+            </View>
+            </View>
+         
             <Button
+                style={styles.searchButton}
                 onPress={this.go2Details}
                 title="Search"
             />
+        
         </View>
     }
 }
+
+const styles = StyleSheet.create({
+    base: {
+        backgroundColor: 'transparent',
+        borderBottomColor: color.gray20,
+        borderBottomWidth: 1 / PixelRatio.get(),
+        justifyContent: 'space-between',
+        flexDirection: 'column',
+        flex:1, // same as 100%
+        alignItems: 'stretch',
+        marginBottom:2
+    },
+    mainBody:{
+        alignItems: 'baseline',
+        flexDirection: 'row',
+        padding: fontSize.small,
+        justifyContent: 'center',
+    },
+    searchItem: {
+        height: 30,
+        justifyContent: 'center',
+        fontSize: fontSize.default
+    },
+    searchOptions:{
+        width:120,
+        marginLeft:10,
+    },
+    searchButton:{
+        
+    }
+});
 
 export default SearchScreen;
