50be5a4f81bb75b27eb0bb9b15289cb92bde1b28
Bo Chen <bochen2014@yahoo.com>
2017-10-02 00:19:47 +1100

    cannot test event

---
+++ __tests__/setup.js
@@ -0,0 +1,6 @@
+//process.env.NODE_ENV = 'production' 
+
+/* file-that-runs-before-all-of-my-tests.js */
+
+// This will mutate `react-native`'s require cache with `react-native-mock`'s.
+//require('react-native-mock/mock'); // <-- side-effects!!!
--- app/home/containers/homeContainer.spec.js
+++ app/home/containers/homeContainer.spec.js
@@ -2,19 +2,37 @@
 import 'react-native';
 import React from 'react';
 import renderer from 'react-test-renderer';
+import { HomeContainer } from './homeContainer';
 
-import { HomeContainer }
-  from './homeContainer';
 
 describe('HomeContainer', () => {
   it('renders correctly', () => {
     const carOfTheWeek = {
       review: 'test review',
     }
-    const navigate = jest.fn();
-    const tree = renderer.create(<HomeContainer carOfTheWeek={carOfTheWeek} 
-          navigation={{ navigate }} isFetching={false} />).toJSON();
+    const tree = renderer.create(<HomeContainer carOfTheWeek={carOfTheWeek}
+      navigation={{}} isFetching={false} />).toJSON();
 
     expect(tree).toMatchSnapshot();
+  });
+
+
+  it.skip('got to playground', () => {
+    const navigate = jest.fn();
+    const getCarOfTheWeek = jest.fn();
+    const testRenderer = renderer.create(
+      <HomeContainer carOfTheWeek={null}
+        getCarOfTheWeek={getCarOfTheWeek}
+        navigation={{ navigate }} isFetching={true} />
+    )
+
+    //https://reactjs.org/docs/test-renderer.html#testinstancefindbytype
+    const testInstance = testRenderer.getInstance();
+    const myBtn = testInstance.findByType('Button') //we only render the fist level components
+    myBtn.simulate('click');
+
+    //assert
+    expect(navigate.mock.calls.length).toBe(1)
+    expect(navigate).toBeCalled()
   });
 });
