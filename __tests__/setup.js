91e1de4e2a19bb687834d4716e842529e1f105a6
Bo Chen <bochen2014@yahoo.com>
2017-10-15 16:06:44 +1100

    no need for setup

--- __tests__/setup.js
+++
@@ -1,6 +0,0 @@
-//process.env.NODE_ENV = 'production' 
-
-/* file-that-runs-before-all-of-my-tests.js */
-
-// This will mutate `react-native`'s require cache with `react-native-mock`'s.
-//require('react-native-mock/mock'); // <-- side-effects!!!
